70275c70e155b497530c54b78b3601570d7f7e45
JeongDay <cidd1332@gmail.com>
2026-01-27 10:55:40 +0900

    2026.01.27 화요일 - 오전시간 2

--- Assets/_Photon/02. Scripts/Fight/HpController.cs
+++ Assets/_Photon/02. Scripts/Fight/HpController.cs
@@ -1,23 +1,40 @@
 ﻿using Photon.Pun;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpController : MonoBehaviourPun
 {
     private Animator anim;
     private IHitbox hitbox;
 
-    public float hp = 100f;
+    public TextMeshPro nicknameText;
+    public Image hpBar;
+
+    public float currentHp = 100f;
+    public float maxHp = 100f;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        if (photonView.IsMine)
+        {
+            nicknameText.text = PhotonNetwork.NickName;
+            nicknameText.color = Color.green;
+        }
+        else
+        {
+            nicknameText.text = photonView.Owner.NickName;
+            nicknameText.color = Color.red;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (hp <= 0)
+        if (currentHp <= 0)
             return;
-        
+
         hitbox = other.GetComponent<IHitbox>();
         if (hitbox != null)
             photonView.RPC("OnGetDamage", RpcTarget.All, hitbox.Damage);
@@ -26,18 +43,20 @@ public class HpController : MonoBehaviourPun
     [PunRPC]
     private void OnGetDamage(float damage)
     {
-        hp -= damage;
-        Debug.Log($"{photonView.Owner.NickName}의 남은 체력 : {hp}");
-        
-        if (hp <= 0)
+        currentHp -= damage;
+
+        hpBar.fillAmount = currentHp / maxHp;
+
+        if (currentHp <= 0)
             Death();
     }
 
     private void Death()
     {
-        Debug.Log($"{photonView.Owner.NickName} Die");
         anim.SetTrigger("Death");
-        
         GetComponent<FightController>().SetDeath();
+
+        if (photonView.IsMine)
+            Fade.onFadeAction?.Invoke(3f, Color.black, true);
     }
 }
